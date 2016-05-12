e71b852f09af0015bc63dfdab2a22882219222d2
Toni Cárdenas <toni@tcardenas.me>
2016-05-12 02:30:38 +0200

    Detect "no Internet" errors, move to Disconnected instead of Failed.

--- Source/ARTRealtime.m
+++ Source/ARTRealtime.m
@@ -786,7 +786,11 @@
         }
     }
 
-    [self transition:ARTRealtimeFailed withErrorInfo:[ARTErrorInfo createWithNSError:error.error]];
+    if (error.type == ARTRealtimeTransportErrorTypeNoInternet) {
+        [self transition:ARTRealtimeDisconnected];
+    } else {
+        [self transition:ARTRealtimeFailed withErrorInfo:[ARTErrorInfo createWithNSError:error.error]];
+    }
 }
 
 - (BOOL)reconnectWithFallback {
